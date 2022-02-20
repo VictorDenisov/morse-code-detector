a54b882eebd690eb2487cd0bdfcec383560a66fc
Victor Denisov <denisovenator@gmail.com>
2022-02-19 21:35:03 -0800

    Cleanup sample comments and redundant code

--- main.go
+++ main.go
@@ -23,36 +23,26 @@ func main() {
 	gc.Cursor(0)
 	stdscr.Keypad(true)
 
-	y, x := 5, 2
-
-	_, err = gc.NewWindow(HEIGHT, WIDTH, y, x)
+	_, err = gc.NewWindow(HEIGHT, WIDTH, 5, 2)
 	if err != nil {
 		log.Fatal("new_window:", err)
 	}
-	stdscr.MovePrint(0, 0, "Use up/down arrow keys, enter to "+
-		"select or 'q' to quit")
-	stdscr.MovePrint(1, 0, "You may also left mouse click on an entry to select")
+	stdscr.MovePrint(0, 0, "Use mouse button to signal morse code or 'q' to quit")
 	stdscr.Refresh()
 
 	if gc.MouseOk() {
 		stdscr.MovePrint(3, 0, "WARN: Mouse support not detected.")
 	}
 
-	gc.MouseInterval(0)
+	gc.MouseInterval(0) // Allow arbitrary duration between press and release of mouse button.
 
-	// If, for example, you are temporarily disabling the mouse or are
-	// otherwise altering mouse button detection temporarily, you could
-	// pass a pointer to a MouseButton object as the 2nd argument to
-	// record that information. Invocation may look something like:
-
-	gc.MouseMask(gc.M_B1_PRESSED|gc.M_B1_RELEASED, nil) // only detect left mouse clicks
+	gc.MouseMask(gc.M_B1_PRESSED|gc.M_B1_RELEASED, nil) // only detect left mouse press and release
 
 	var key gc.Key
 	for key != 'q' {
 		key = stdscr.GetChar()
 		switch key {
 		case gc.KEY_MOUSE:
-			/* pull the mouse event off the queue */
 			if md := gc.GetMouse(); md != nil {
 				if md.State == gc.M_B1_PRESSED {
 					stdscr.MovePrintf(22, 0, "Mouse pressed = %3d/%c", key, key)
@@ -66,6 +56,5 @@ func main() {
 			stdscr.ClearToEOL()
 			stdscr.Refresh()
 		}
-
 	}
 }
